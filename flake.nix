534b4f8fe8be75b195300033905ba58fbea106e9
mugahed izzeldin <mugahedman@hotmail.com>
2025-09-14 22:27:23 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ nixpkgs, home-manager, ... }:
+    {
+      nixosConfigurations = {
+        "nixos" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            hosts/default/configuration.nix
+	    home-manager.nixosModules.home-manager
+	    {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.mugahed = hosts/default/home.nix;
+	    home-manager.backupFileExtension = "backup";
+
+	    }
+          ];
+        };
+      };
+    };
+}
---
+++ modules/home-manager/i3wm.nix
@@ -0,0 +1,18 @@
+{ inputs, pkgs, ... }:
+
+{
+
+ xsession.windowManager.i3 = {
+    enable = true;
+    package = pkgs.i3-gaps;
+    config = {
+      modifier = "Mod4";
+      gaps = {
+        inner = 10;
+        outer = 5;
+      };
+      terminal = "konsole";
+    };
+  };
+
+}
