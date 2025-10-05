b608e1a034eddca9904da61d932aec6e9237d960
mugaizzo <mugahedman@hotmail.com>
2025-10-05 16:55:00 -0600

    Version 55 at 2025-10-05 16:55:00

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,21 +1,20 @@
 _: {
   imports = [
-#    ./boot.nix
-#    ./hardware.nix
-#    ./security.nix
-#    ./lanzaboote.nix
-#    ./services.nix
+    #    ./boot.nix
+    #    ./hardware.nix
+    #    ./security.nix
+    #    ./lanzaboote.nix
+    ./services.nix
     ./system.nix
-#    ./ld.nix
-#    ./network.nix
-#    ./virtualisation.nix
-#    ./xdgPortal.nix
-#    ./greetd.nix
+    #    ./ld.nix
+    #    ./network.nix
+    #    ./virtualisation.nix
+    #    ./xdgPortal.nix
+    #    ./greetd.nix
     ./user.nix
-#    ./zram.nix
+    #    ./zram.nix
     ./fonts.nix
-#    ./steam.nix
-#    ./btrfs.nix
+    #    ./steam.nix
+    #    ./btrfs.nix
   ];
 }
-
