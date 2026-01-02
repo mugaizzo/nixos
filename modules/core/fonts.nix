8dd5d271a820929540ee6c48784b578cdb561170
mugaizzo <mugahedman@hotmail.com>
2026-01-02 00:02:41 -0700

    Version 93 at 2026-01-02 00:02:40

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -3,6 +3,12 @@
     enableDefaultPackages = true;
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
+      noto-fonts-color-emoji
+      nerd-fonts.fira-code            
+      nerd-fonts.hack
+      nerd-fonts.meslo-lg
+
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
